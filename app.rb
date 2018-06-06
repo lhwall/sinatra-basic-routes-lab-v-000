9ef8158a78ecdd2b3554cc91f7698c8efbc34108
Lucy Wall <lhwall@gmail.com>
2018-06-06 14:29:46 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Lucy"
+  end
+
+  get "/hometown" do
+    "My hometown is Silver Spring"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Bad Romance"
+  end
+
 end
